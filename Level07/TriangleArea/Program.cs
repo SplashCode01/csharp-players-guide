1acbee7b987c79b5a70b42e2c92fc3ee2eef901e
SplashCode <shota.goginashvili@gmail.com>
2026-07-02 10:58:14 +0200

    Level 7: triangle area

---
+++ Level07/TriangleArea/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Welcome, Triangle Farmer!");
+Console.WriteLine("I see you need some help calculating your triangle area.");
+Console.WriteLine("Worry not! Input your base size and height below, and let me do the rest:");
+double triangleBase = Convert.ToDouble(Console.ReadLine());
+double triangleHeight = Convert.ToDouble(Console.ReadLine());
+
+double area = (triangleBase * triangleHeight) / 2;
+
+Console.WriteLine($"The area of your triangle is: {area}.");
+Console.ReadKey();
