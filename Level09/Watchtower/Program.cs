76222f30f2f245ea404f26f8b7c33cf858e4bb33
SplashCode <shota.goginashvili@gmail.com>
2026-07-02 13:18:52 +0200

    Level 9: Watchtower

---
+++ Level09/Watchtower/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Watchtower;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Watchtower";
+
+        Console.WriteLine("Soldier! Grab your spyglass and map and tell me where the enemies are!");
+        Console.Write("\nWhat is their horizontal position? ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("What is their vertical position? ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        if (x == 0 & y == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The enemies are at our doorstep!");
+        }
+        else if (x == 0)
+        {
+            if (y > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The enemies are to the North!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The enemies are to the South!");
+            }
+        }
+        else if (y == 0)
+        {
+            if (x > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The enemies are to the East!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The enemies are to the West!");
+            }
+        }
+        else if (x > 0 & y > 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("The enemies are to the North-East!");
+        }
+        else if (x < 0 & y > 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("The enemies are to the North-West!");
+        }
+        else if (x < 0 & y < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("The enemies are to the South-West!");
+        }
+        else if (x > 0 & y < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("The enemies are to the South-East!");
+        }
+
+        Console.ReadKey();
+    }
+}
