58f12dc01b769b4cc5a4b07d078a472274d174a4
SplashCode <shota.goginashvili@gmail.com>
2026-07-02 14:05:51 +0200

    Level 11: The Magic Cannon

---
+++ Level11/TheMagicCannon/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TheMagicCannon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "The Magic Cannon";
+
+        Console.WriteLine("Hello, Artillerymen! Let me calculate you the gem combinations.");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{i}: Greek Fire");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{i}: Fire");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{i}: Electric");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"{i}: Normal");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
