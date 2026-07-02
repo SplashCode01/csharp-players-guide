93df8d52aba2c2bdb7b2915204fce1a5ebfa5b52
SplashCode <shota.goginashvili@gmail.com>
2026-07-02 11:17:28 +0200

    Level 7: Duckbear Remainder

---
+++ Level07/DuckbearRemainder/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DuckbearRemainder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Sisters! Do you need some arbitering?");
+        Console.WriteLine("Enter the number of chocolate eggs below, and I will divide everything!");
+        int chocoEggs = Convert.ToInt32(Console.ReadLine());
+        int remainder = chocoEggs % 4;
+        chocoEggs -= remainder;
+        chocoEggs /= 4;
+        Console.WriteLine($"Sisters! You will each receive {chocoEggs} eggs!");
+        Console.WriteLine($"Your bear shall receive {remainder}");
+        Console.ReadKey();
+    }
+}
