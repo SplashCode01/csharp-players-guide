2baab5b11fed51015b495f5c76e7d1788f64b5a3
SplashCode <shota.goginashvili@gmail.com>
2026-07-02 11:28:25 +0200

    Level 7: Vizier Calculator

---
+++ Level07/VizierPointCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Welcome, my liege! I am but your humble vizier, here to help.");
+Console.WriteLine("Let me help calculate your points.");
+Console.WriteLine("How many estates does your eminence hold?");
+int estates = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many duchies does your grace hold?");
+int duchies = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many provinces does your majesty hold?");
+int provinces = Convert.ToInt32(Console.ReadLine());
+
+int score = estates * 1 + duchies * 3 + provinces * 6;
+Console.WriteLine($"My liege! You have an unbelievable {score} points!");
+Console.WriteLine("You truly possess the greatest realm in the world!");
+Console.ReadKey();
