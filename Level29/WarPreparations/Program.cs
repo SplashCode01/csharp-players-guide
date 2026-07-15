5b8061dba36603b8e093840443b4433e501005c6
SplashCode <shota.goginashvili@gmail.com>
2026-07-15 12:02:46 +0200

    Level 29: War Preparations

---
+++ Level29/WarPreparations/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WarPreparations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sword basicSword = new Sword(Material.Wood, Gemstone.None, 1);
+        Sword steelSword = basicSword with { Material = Material.Steel };
+        Sword broadSword = steelSword with { Width = 3 };
+
+        Console.WriteLine(basicSword);
+        Console.WriteLine(steelSword);
+        Console.WriteLine(broadSword);
+
+        Console.ReadKey();
+    }
+}
+
+public record Sword(Material Material, Gemstone Gemstone, int Width);
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gemstone { Emerald, Amber, Sapphire, Diamond, Bitstone, None }
