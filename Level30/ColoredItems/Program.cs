b5d33ba59e6efc9fc8178d3a56dd8514f8c8ef33
SplashCode <shota.goginashvili@gmail.com>
2026-07-15 12:13:20 +0200

    Level 30: Colored Items

---
+++ Level30/ColoredItems/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ColoredItems;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+        ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+        ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+        sword.Display();
+        bow.Display();
+        axe.Display();
+
+        Console.ReadKey();
+    }
+}
+
+public class ColoredItem<T>
+{
+    public T Item { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item);
+    }
+}
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
